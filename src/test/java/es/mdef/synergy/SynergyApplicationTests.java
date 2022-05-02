d6658730d7f7019ec0a028862a9c0d3a3217ab7c
Victor <victor@lobaco.es>
2022-05-02 05:05:11 +0200

    Setting Up Spring Project

---
+++ src/test/java/es/mdef/synergy/SynergyApplicationTests.java
@@ -0,0 +1,13 @@
+package es.mdef.synergy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SynergyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
